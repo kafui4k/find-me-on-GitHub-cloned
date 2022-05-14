b85df50ed257508a9317479d9b2161594dd9eb7c
Kafui Alordo <kalordo7@gmail.com>
2022-05-14 13:47:49 +0000

    Implement Home.js and NotFound.js Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layouts/NavBar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import {Home} from './components/pages/Home';
 import {Alert} from './components/layouts/Alert';
 import {About} from './components/pages/About';
 import User from './components/users/User';
+import {NotFound} from './components/pages/NotFound';
 
 import GitHubState from './context/github/GitHubState';
 import AlertState from './context/alert/AlertState';
@@ -21,22 +21,14 @@ const App = () => {
                         <div className='container'>
                             <Alert />
                             <Switch>
-                                <Route
-                                    exact
-                                    path='/'
-                                    render={(props) => (
-                                        <Fragment>
-                                            <Search />
-                                            <Users />
-                                        </Fragment>
-                                    )}
-                                />
+                                <Route exact path='/' component={Home} />
                                 <Route exact path='/about' component={About} />
                                 <Route
                                     exact
                                     path='/user/:login'
                                     component={User}
                                 />
+                                <Route component={NotFound} />
                             </Switch>
                         </div>
                     </div>
